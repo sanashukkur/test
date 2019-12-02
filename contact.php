e0986b25ce3ed7cfce2e6530e393cda46086978c
hakkem pm <hakkempalakkal@gmail.com>
2019-12-03 00:35:12 +0530

    cargo

---
+++ contact.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from designarc.biz/demos/logic-cargo/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 25 Nov 2019 07:57:27 GMT -->
+<head>
+	<meta charset="UTF-8">
+	<title>Logis Cargo - We are transport network - HTML 5 Template</title>
+	<!-- viewport meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- styles -->
+	<!-- helpers -->
+	<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+	<!-- fontawesome css -->
+	<link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.min.css">
+	<!-- strock gap icons -->
+	<link rel="stylesheet" href="plugins/Stroke-Gap-Icons-Webfont/style.css">
+	<!-- revolution slider css -->
+	<link rel="stylesheet" href="plugins/revolution/css/settings.css">
+	<link rel="stylesheet" href="plugins/revolution/css/layers.css">
+	<link rel="stylesheet" href="plugins/revolution/css/navigation.css">
+	<!-- flaticon css -->
+	<link rel="stylesheet" href="plugins/flaticon/flaticon.css">
+	<!-- jQuery ui css -->
+	<link href="plugins/jquery-ui-1.11.4/jquery-ui.css" rel="stylesheet">
+	<!-- owl carousel css -->
+	<link rel="stylesheet" href="plugins/owl.carousel-2/assets/owl.carousel.css">
+	<link rel="stylesheet" href="plugins/owl.carousel-2/assets/owl.theme.default.min.css">
+	<!-- animate css -->
+	<link rel="stylesheet" href="plugins/animate.min.css">
+	<!-- fancybox -->
+	<link rel="stylesheet" href="plugins/fancyapps-fancyBox/source/jquery.fancybox.css">
+
+
+
+	<!-- master stylesheet -->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- responsive stylesheet -->
+	<link rel="stylesheet" href="css/responsive.css">
+
+
+
+</head>
+<body>
+
+<!-- Start header -->
+<header id="header" class="stricky">
+	<div class="thm-container clearfix">
+		<div class="logo pull-left">
+			<a href="index.html">
+				<img src="images/logo.png" alt="">
+			</a>
+		</div>
+		<div class="header-info pull-right">
+			
+			<div class="info-box">
+				<div class="icon-box no-border">
+					<i class="icon icon-Timer"></i>
+				</div>
+				<div class="text-box">
+					<p class="highlighted">Mon - Sat 9.00 - 18.00</p>
+					<p>Sunday Closed</p>
+				</div>
+			</div>
+			<div class="info-box">
+				<div class="icon-box">
+					<i class="icon icon-Phone2"></i>
+				</div>
+				<div class="text-box">
+					<p class="highlighted">Call Us:</p>
+					<p class="phone-number">(123) 456 7890</p>
+				</div>
+			</div>
+			<div class="info-box search-box-wrapper">
+				<div class="icon-box">
+					<i class="flaticon-search51"></i>
+				</div>
+				<form action="#" class="search-box-holder">
+					<div class="search-box">
+						<div class="form">
+							<input type="text" placeholder="Track Your Shipment">
+							<button type="submit"><span>GO</span></button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</header>
+<!-- End header  -->
+
+<!-- Start mainmenu -->
+<nav class="main-menu-wrapper stricky">
+	<div class="thm-container menu-gradient ">
+		<div class="clearfix">
+			<div class="nav-holder pull-left">
+				<div class="nav-header">
+					<button><i class="fa fa-bars"></i></button>
+				</div>
+				<div class="nav-footer">
+					<ul class="nav">
+						<li class="has-submenu">
+					<a href="index.html">Home</a>
+					
+				</li>
+						<li class="has-submenu">
+							<a href="about.html">about us</a>
+						
+						</li>
+						<li class="has-submenu">
+							<a href="service.html">Services</a>
+							<ul class="submenu">
+								<li><a href="single-service.html">Service 1</a></li>
+								<li><a href="pricing.html">Service 2</a></li>
+							</ul>
+						</li>
+						<li class="has-submenu">
+							<a href="gallery.html">Gallery</a>
+						</li>
+						
+						<li><a href="contact.html">contact us</a></li>
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</nav>
+<!-- End mainmenu -->
+
+<section class="inner-banner">
+	<div class="thm-container">
+		<h2>Contact Us</h2>
+		<ul class="breadcumb">
+			<li><a href="index.html"><i class="fa fa-home"></i> Home</a></li>
+			<li><span>Contact Us</span></li>
+		</ul>
+	</div>
+</section>
+
+<section class="sec-padding contact-page-content">
+	<div class="thm-container">
+		<div class="sec-title">
+			<h2><span>Get in touch</span></h2>
+			<p>You can talk to our online representative at any time. Please use our Live Chat System on our website or <br>Fill up below instant messaging programs.</p>
+		</div>
+		<div class="row">
+			<div class="col-md-7 col-sm-6 col-xs-12 pull-left">
+				<form method="post" action="sendmail.php" class="contact-form contact-page">
+					<p><input type="text" placeholder="Name" required name="name"></p>
+					<p><input type="text" placeholder="Email" required name="email"></p>
+					<p><input type="text" placeholder="Subject"required   name="subject"></p>
+					<p><textarea name="message" placeholder="Message" required ></textarea></p>
+					<button type="submit" name="submit" class="thm-btn">Submit Now <i class="fa fa-arrow-right"></i></button>
+				</form>
+				<!-- <h3><?php include "sendmail.php"?></h3> -->
+			</div>
+			<div class="col-md-4 col-sm-6 col-xs-12 pull-right">
+				<div class="contact-info">
+					<ul>
+						<li>
+							<div class="icon-box">
+								<i class="icon icon-Pointer"></i>
+							</div>
+							<div class="content">
+								<p>	
+Golden cargo service <br> 63/3138A.karshaka Road Kizhake Kavadam, <br>Cochin-16</p>
+							</div>
+						</li>
+						<li>
+							<div class="icon-box">
+								<i class="icon icon-Plaine"></i>
+							</div>
+							<div class="content">
+								<p>	
+contact@goldencargoservice.com</p>
+							</div>
+						</li>
+						<li>
+							<div class="icon-box">
+								<i class="icon icon-Phone2"></i>
+							</div>
+							<div class="content">
+								<p>989577 8143 <br>+0120  987 65419</p>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">	
+				</br>
+				<div class="mapouter"><div class="gmap_canvas"><iframe width="1080" height="1080" id="gmap_canvas" src="https://maps.google.com/maps?q=Golden%20cargo%20service%2063%2F3138A.karshaka%20Road%20Kizhake%20Kavadam%2C%20Cochin-16&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net">embedgooglemap.net</a></div><style>.mapouter{position:relative;text-align:right;height:300px;width:1024px;}.gmap_canvas {overflow:hidden;background:none!important;height:300px;width:1080px;}</style></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="footer-top">
+	<div class="thm-container">
+		<div class="row">
+			
+			<p> Get in touch</p>
+			<div class="col-md-8">
+				<div class="col-md-6">
+					<h1 class="footer_h1">989577 8143<br>
+984799 1165</h1>
+				
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<footer id="footer" class="sec-padding">
+	<div class="thm-container">
+		<div class="row">
+			<div class="col-md-3 col-sm-6 footer-widget">
+				<div class="about-widget">
+					<a href="#"><img src="images/footer-logo.png" alt="Awesome Image"/></a>
+					<p>Lorem ipsum dolor sit amet, consecte- tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+					<a href="#">Read More <i class="fa fa-angle-double-right"></i></a>
+					<ul class="social">
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 footer-widget">
+				<div class="pl-30">
+					<div class="title">
+						<h3><span>OUR SERVICES</span></h3>
+					</div>
+					<ul class="link-list">
+						<li><a href="#">Lorem ipsum</a></li>
+						<li><a href="#">Lorem ipsum</a></li>
+						<li><a href="#">Lorem ipsum</a></li>
+						<li><a href="#">Lorem ipsum</a></li>
+						<li><a href="#">Lorem ipsum</a></li>
+						<li><a href="#">Lorem ipsum</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-2 col-sm-6 footer-widget">
+				<div class="title">
+					<h3><span>Quick Links</span></h3>
+				</div>
+				<ul class="nav">
+						<li class="has-submenu">
+					<a href="index.html">Home</a>
+					
+				</li>
+						<li class="has-submenu">
+							<a href="about.html">about us</a>
+						
+						</li>
+						<li class="has-submenu">
+							<a href="service.html">Services</a>
+							<ul class="submenu">
+								<li><a href="single-service.html">Service 1</a></li>
+								<li><a href="pricing.html">Service 2</a></li>
+							</ul>
+						</li>
+						<li class="has-submenu">
+							<a href="gallery.html">Gallery</a>
+						</li>
+						
+						<li><a href="contact.php">contact us</a></li>
+					</ul>
+			</div>
+			<div class="col-md-4 col-sm-6 footer-widget">
+				<div class="title">
+					<h3><span>Quick Links</span></h3>
+				</div>
+				<ul class="contact-infos">
+					<li>
+						<div class="icon-box">
+							<i class="fa fa-map-marker"></i>
+						</div>
+						<div class="text-box">
+							<p><b>Golden cargo service</b> <br> 
+
+63/3138A.karshaka Road
+Kizhake Kavadam, Cochin-16
+
+							
+							</p>
+						</div>
+					</li>
+					<li>
+						<div class="icon-box">
+							<i class="fa fa-envelope-o"></i>
+						</div>
+						<div class="text-box">
+							<p>contact@goldencargoservice.com</p>
+						</div>
+					</li>
+					<li>
+						<div class="icon-box">
+							<i class="fa fa-phone"></i>
+						</div>
+						<div class="text-box">
+							<p>
+989577 8143  /  984799 1165</p>
+						</div>
+					</li>
+					
+					
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<section class="bottom-bar">
+	<div class="thm-container clearfix">
+		<div class="pull-left">
+			<p>Copyright © Logic Cargo 2016. All rights reserved.</p>
+		</div>
+		<div class="pull-right">
+			<p>Created by: Design</p>
+		</div>
+	</div>
+</section>
+
+<!-- jQuery js -->
+<script src="plugins/jquery/jquery-1.11.3.min.js"></script>
+<!-- bootstrap js -->
+<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+<!-- jQuery ui js -->
+<script src="plugins/jquery-ui-1.11.4/jquery-ui.js"></script>
+<!-- owl carousel js -->
+<script src="plugins/owl.carousel-2/owl.carousel.min.js"></script>
+<!-- jQuery appear -->
+<script src="plugins/jquery-appear/jquery.appear.js"></script>
+<!-- jQuery countTo -->
+<script src="plugins/jquery-countTo/jquery.countTo.js"></script>
+<!-- jQuery validation -->
+<script src="plugins/jquery-validation/dist/jquery.validate.min.js"></script>
+<!-- gmap.js helper -->
+<script src="http://maps.google.com/maps/api/js"></script>
+<!-- gmap.js -->
+<script src="plugins/gmap.js"></script>
+<!-- mixit up -->
+<script src="plugins/jquery.mixitup.min.js"></script>
+
+<!-- revolution slider js -->
+<script src="plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
+<script src="plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
+
+<!-- fancy box -->
+<script src="plugins/fancyapps-fancyBox/source/jquery.fancybox.pack.js"></script>
+<!-- type script -->
+<script src="plugins/typed.js-master/dist/typed.min.js"></script>
+
+<!-- theme custom js  -->
+<script src="js/main.js"></script>
+
+
+
+</body>
+
+<!-- Mirrored from designarc.biz/demos/logic-cargo/contact.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 25 Nov 2019 07:57:27 GMT -->
+</html>
